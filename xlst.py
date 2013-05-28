068e1b8a96fe4d29c4703bc42ee8b6ff6aaafdf6
robi-wan <robi-wan@suyu.de>
2013-05-28 08:26:56 +0200

    Use codecs package for file writing.
    Use codecs to write files - so encoding happens for all strings written.
    Mark strings with u for correct writing.
    
    Function __cell_range to check for maximum number of rows - so we do not
    try to access cells 'after' the end of the sheet.
    
    Easier check for existence of read data - remove len() construct.
    
    Add some comments.

--- xlst.py
+++ xlst.py
@@ -3,12 +3,13 @@
 
 import argparse
 import os
+import codecs
 import xlrd
 
 
 class MPS3(object):
 
-    sheet_index = 3
+    sheet_index = 3  # 'ini903'
     # rows and columns are zero based
     column = 0
     start_row = 9
@@ -23,17 +24,18 @@ class MPS3(object):
         sheet = self.book.sheet_by_index(self.sheet_index)
         for row in xrange(self.start_row, sheet.nrows):
             text = sheet.cell(row, col).value
-            if len(text) > 0:
+            if text:
                 self.values.append(text)
 
 
 class HMI(MPS3):
 
-    sheet_index = 4
+    sheet_index = 4  # 'iniHMI'
 
     def __init__(self, book):
         if book.sheet_by_index(self.sheet_index):
             super(HMI, self).__init__(book)
+
 
 class Generator(object):
 
@@ -51,12 +53,9 @@ class MPS3Generator(Generator):
 
     def _write(self):
         f = os.path.join(self.path, self.output_name)
-        with open(f, 'wb') as mps_ini:
+        with codecs.open(f, mode='w', encoding=self.encoding) as mps_ini:
             for value in self.values:
-                #if value.startswith('['):
-                    #mps_ini.write('\n')
-                mps_ini.write(value.encode(self.encoding))
-                mps_ini.write('\n')
+                mps_ini.write(u"{}\n".format(value))
 
 
 class HMIGenerator(MPS3Generator):
@@ -83,7 +82,7 @@ class OutputObject(object):
 class Parameter(OutputObject):
 
     comment = u'//Parametertexte'
-    ini_key = 'PARAM'
+    ini_key = u'PARAM'
 
     def __init__(self, note=None, **kwargs):
         self.note = note
@@ -93,37 +92,37 @@ class Parameter(OutputObject):
 class Category(OutputObject):
 
     comment = u'//Texte Tabelle/Registerkarte'
-    ini_key = 'TAB'
+    ini_key = u'TAB'
 
 
 class Header(OutputObject):
 
     comment = u'//Überschriften Spalten'
-    ini_key = 'COL'
+    ini_key = u'COL'
 
 
 class Menu(OutputObject):
 
     comment = u'//MenüTexte'
-    ini_key = 'MENU'
+    ini_key = u'MENU'
 
 
 class SystemMessages(OutputObject):
 
     comment = u'//Systemtexte(Beschriftungen, Überschriften, usw.)'
-    ini_key = 'SYSTEM'
+    ini_key = u'SYSTEM'
 
 
 class ErrorMessages(OutputObject):
 
     comment = u'//Fehlertexte'
-    ini_key = 'ERROR'
+    ini_key = u'ERROR'
 
 
 class HMICategory(OutputObject):
 
     comment = u'//Texte Registerkarte HMI'
-    ini_key = 'TABHMI'
+    ini_key = u'TABHMI'
 
 
 class Translation(object):
@@ -167,19 +166,19 @@ class Translation(object):
 
     def _categories(self, sheet, lang):
         start_row = 1349
-        for row in xrange(start_row, start_row + 20):
-            cat_name = sheet.cell(row, self.param_name_col).value
-            if len(cat_name) > 0:
+        for row in self.__cell_range(start_row, 20, sheet):
+            name = sheet.cell(row, self.param_name_col).value
+            if name:
                 index = row - start_row
-                self.values.setdefault((lang, 'categories'), []).append(Category(name=cat_name, number=index))
+                self.values.setdefault((lang, 'categories'), []).append(Category(name=name, number=index))
             else:
                 break
 
     def _column_header(self, sheet, lang):
         start_row = 1369
-        for row in xrange(start_row, start_row + 10):
+        for row in self.__cell_range(start_row, 10, sheet):
             name = sheet.cell(row, self.param_name_col).value
-            if len(name) > 0:
+            if name:
                 index = row - start_row
                 self.values.setdefault((lang, 'header'), []).append(Header(name=name, number=index))
             else:
@@ -187,9 +186,9 @@ class Translation(object):
 
     def _menues(self, sheet, lang):
         start_row = 1379
-        for row in xrange(start_row, start_row + 30):
+        for row in self.__cell_range(start_row, 30, sheet):
             name = sheet.cell(row, self.param_name_col).value
-            if len(name) > 0:
+            if name:
                 index = row - start_row
                 self.values.setdefault((lang, 'menues'), []).append(Menu(name=name, number=index))
             else:
@@ -197,9 +196,9 @@ class Translation(object):
 
     def _system_messages(self, sheet, lang):
         start_row = 1409
-        for row in xrange(start_row, start_row + 50):
+        for row in self.__cell_range(start_row, 50, sheet):
             name = sheet.cell(row, self.param_name_col).value
-            if len(name) > 0:
+            if name:
                 index = row - start_row
                 self.values.setdefault((lang, 'system_messages'), []).append(SystemMessages(name=name, number=index))
             else:
@@ -207,9 +206,9 @@ class Translation(object):
 
     def _error_messages(self, sheet, lang):
         start_row = 1459
-        for row in xrange(start_row, self.__rows(sheet, start_row + 20)):
+        for row in self.__cell_range(start_row, 20, sheet):
             name = sheet.cell(row, self.param_name_col).value
-            if len(name) > 0:
+            if name:
                 index = row - start_row
                 self.values.setdefault((lang, 'error_messages'), []).append(ErrorMessages(name=name, number=index))
             else:
@@ -217,16 +216,19 @@ class Translation(object):
 
     def _hmi_categories(self, sheet, lang):
         start_row = 1319
-        for row in xrange(start_row, start_row + 30):
+        for row in self.__cell_range(start_row, 30, sheet):
             name = sheet.cell(row, self.param_name_col).value
-            if len(name) > 0:
+            if name:
                 index = row - start_row
                 self.values.setdefault((lang, 'hmi_categories'), []).append(HMICategory(name=name, number=index))
             else:
                 break
 
-    def __rows(self, sheet, rows):
+    def __max_rows(self, sheet, rows):
         return min(rows, sheet.nrows)
+
+    def __cell_range(self, start, length, sheet):
+        return xrange(start, self.__max_rows(sheet, start + length))
 
 
 def description_ranges():
@@ -237,6 +239,7 @@ def description_ranges():
 class TranslationGenerator(Generator):
 
     suffix = '.lng'
+    data = ('params', 'categories', 'header', 'menues', 'system_messages', 'error_messages', 'hmi_categories')
 
     def __init__(self, lang, values, path):
         self.languages = lang
@@ -247,20 +250,15 @@ class TranslationGenerator(Generator):
     def _write(self):
         for lang in self.languages:
             f = os.path.join(self.path, "{}{}".format(lang, self.suffix))
-            with open(f, 'wb') as lang_file:
-                lang_file.write("[{}]".format(lang))
-                lang_file.write('\n')
-                data = ('params', 'categories', 'header', 'menues',
-                        'system_messages', 'error_messages', 'hmi_categories')
-                for values in [self.values.get((lang, d)) for d in data]:
+            with codecs.open(f, mode='w', encoding=self.encoding) as lang_file:
+                lang_file.write(u"[{}]\n".format(lang))
+                for values in [self.values.get((lang, d)) for d in self.data]:
                     if values:  # values for 'hmi_categories' may be None
-                        lang_file.write(values[0].comment.encode(self.encoding))
-                        lang_file.write('\n')
+                        lang_file.write(u"{}\n".format(values[0].comment))
                         for value in values:
-                            lang_file.write(str(value))
-                            lang_file.write('\n')
+                            lang_file.write(u"{}{}={}\n".format(value.ini_key, value.number, value.name))
 
-                        lang_file.write('\n')
+                        lang_file.write(u'\n')
 
                 self._write_notes(lang)
 
@@ -268,21 +266,19 @@ class TranslationGenerator(Generator):
         for i in range(len(description_ranges())):
             ran = description_ranges()[i]
             f = os.path.join(self.path, "{}{}{}".format(lang, i+1, self.suffix))
-            with open(f, 'wb') as desc_file:
-                desc_file.write("[{}]".format(lang.upper()))
-                desc_file.write('\n')
+            with codecs.open(f, mode='w', encoding=self.encoding) as desc_file:
+                desc_file.write(u"[{}]\n".format(lang.upper()))
                 for n in ran:
-                    desc_file.write("HILFEPARAM{}={}".format(n, self.__note(lang, n)))
-                    desc_file.write('\n')
+                    desc_file.write(u"HILFEPARAM{}={}\n".format(n, self.__note(lang, n)))
 
     def __delimiter(self):
-        return u'§§'.encode(self.encoding)
+        return u'§§'
 
     def __note(self, lang, number):
         params = self.values.get((lang, 'params'))
         for p in params:
             if p.number == number and p.note:
-                note = p.note.text.encode(self.encoding)
+                note = p.note.text
                 note = self.__delimiter().join(note.splitlines())
                 return note
         return None
